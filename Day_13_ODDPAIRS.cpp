b4006051aaf30a044c7f7555db3f39b34e41a9ba
pranshul2004 <116330932+pranshul2004@users.noreply.github.com>
2023-09-30 23:16:37 +0530

    Add files via upload

---
+++ Day_13_ODDPAIRS.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long int n;
+        cin>>n;
+        cout<<(n*n)/2<<endl;
+        // long long int count=0;
+        // for(int i=1 ; i<n ; i++){
+        //     for(int j=i+1 ; j<=n ; j+=2){
+        //         // if((i+j) % 2!=0)
+        //             count+=2;
+        //     }
+        // }
+        // std::cout << count << std::endl;
+    }
+}
