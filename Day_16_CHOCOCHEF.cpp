30a1bcd3c2dc7f9628c70c2ce78ac6959f488040
pranshul2004 <116330932+pranshul2004@users.noreply.github.com>
2023-10-03 21:06:24 +0530

    Add files via upload

---
+++ Day_16_CHOCOCHEF.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        std::cout << 1<<" "<<1<<" "<<n-2 << std::endl;
+    }
+}
